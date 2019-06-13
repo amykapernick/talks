76d51f29d93bbd5ef52a017d55adb07a5c7e6826
Amy Kapernick <amy@aimhigherwebdesign.com>
2019-06-14 00:12:18 +0800

    working on oslo talk

--- Gruntfile.js
+++ Gruntfile.js
@@ -158,7 +158,7 @@ module.exports = grunt => {
 	grunt.registerTask( 'default', [ 'css', 'js' ] );
 
 	// JS task
-	grunt.registerTask( 'js', [ 'jshint', 'uglify', 'qunit' ] );
+	grunt.registerTask( 'js', [ 'jshint', 'uglify' ] );
 
 	// Theme CSS
 	grunt.registerTask( 'css-themes', [ 'sass:themes' ] );
